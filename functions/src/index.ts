08c660c6b438741b09e0fb74435738d59d97ea58
andoshin11 <andoshin11@mercari.com>
2018-05-01 18:58:18 +0900

    Made it better

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,9 @@
 import * as functions from 'firebase-functions';
 import fetch from 'node-fetch';
+import * as Google from 'googleapis-async';
+import TwitterClient from './twitter';
 
-const SOURCE = "https://github.com/andoshin11/social-profile/raw/master/profile.json"
+const SOURCE = "https://github.com/andoshin11/social-profile/raw/master/profile.json";
 
 enum Service {
 	TWITTER = "twitter",
@@ -22,10 +24,71 @@ const getProfile = async (): Promise<Profile> => {
 	return json;
 }
 
-export const testFunc = functions.https.onRequest(async (req, res) => {
+/**
+ * getAccessToken function
+ *
+ * @param {Object} req Cloud Function request context
+**/
+const getAccessToken = (req) => {
+  const header = req.get('Authorization')
+  if (header) {
+      const match = header.match(/^Bearer\s+([^\s]+)$/);
+      if (match) {
+          return match[1];
+      }
+  }
+  return null;
+}
+
+/**
+ * isValidUser function
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ * @return {boolean} whether the user is valid
+ */
+const isValidUser = async (req, res): Promise<boolean> => {
+  const accessToken = getAccessToken(req);
+
+  if (!accessToken) return false;
+
+  const auth = new Google.auth.OAuth2();
+
+  // Set credential
+  auth.setCredentials({ access_token: accessToken });
+
+  const bucket = functions.config().bucket.pac;
+  const permission = 'storage.buckets.get';
+  const options = {
+    bucket,
+    permissions: [permission],
+    auth
+  }
+
   try {
-    const profile = await getProfile();
-    res.send(profile[Service.TWITTER]);
+    const response = await Google.storage('v1').buckets.testIamPermissions(options)
+    if (response && response['permissions'] && response['permissions'].includes(permission)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
+export const updateProfile = functions.https.onRequest(async (req, res) => {
+  try {
+    const isValid = await isValidUser(req, res)
+    if (isValid) {
+      const profile = await getProfile();
+      const client = new TwitterClient();
+
+      await client.updateProfile(profile[Service.TWITTER]);
+      res.send('success');
+    } else {
+      res.status(403).send("The req is forbidden.");
+    }
   } catch(e) {
     console.log(e)
   }
